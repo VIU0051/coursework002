04ff12941c55672c4e129797e6367e74270e0311
CTA0036 <cta0036@my.londonmet.ac.uk>
2026-04-18 20:46:13 +0100

    Create artist.java

---
+++ artist.java
@@ -0,0 +1,35 @@
+package com.mycompany.musiccoursework;
+
+import java.io.Serializable;
+
+public class Artist implements Serializable
+{
+    private String name;
+    private String genre;
+
+    public Artist(String name, String genre)
+    {
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
+}
